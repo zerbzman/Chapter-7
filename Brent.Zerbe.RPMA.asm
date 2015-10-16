8157c3d8cf4f216d1e3f4e2d50b51b2b5f4eb724
Brent Zerbe <zerbzman@gmail.com>
2015-10-15 23:48:15 -0400

    Format

--- Brent.Zerbe.RPMA.asm
+++ Brent.Zerbe.RPMA.asm
@@ -18,11 +18,11 @@ WhileGreater:
   cmp  ebx, 0         ; if B > 0 continue
   jz   Equal          ; else Exit
   shr  ebx, 1         ; if B is odd continue
-  jnc   EvenN          ; else goto EvenN
+  jnc  EvenN          ; else goto EvenN
   add  Result, eax    ; add A to Result
 EvenN:
   shl  eax, 1         ; A = A*2
-  ;shr  ebx, 1         ; B = B/2 don't need this because I'm doing it early
+  ;shr  ebx, 1        ; B = B/2 don't need this because I'm doing it early
   loop WhileGreater
 
 Equal:                ; Exit if B == 0
