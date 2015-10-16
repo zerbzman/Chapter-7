6dcc55939d27e900448c4b251e6da80ac848ecb4
Brent Zerbe <zerbzman@gmail.com>
2015-10-15 23:46:25 -0400

    Refactored
    Went back to the shift right method I used before to check if B was odd
    but ditched the extra register this time. One less command.

--- Brent.Zerbe.RPMA.asm
+++ Brent.Zerbe.RPMA.asm
@@ -17,12 +17,12 @@ mov ebx, B
 WhileGreater:
   cmp  ebx, 0         ; if B > 0 continue
   jz   Equal          ; else Exit
-  test ebx, 1         ; if B is odd continue
-  jz   EvenN          ; else goto EvenN
+  shr  ebx, 1         ; if B is odd continue
+  jnc   EvenN          ; else goto EvenN
   add  Result, eax    ; add A to Result
 EvenN:
   shl  eax, 1         ; A = A*2
-  shr  ebx, 1         ; B = B/2
+  ;shr  ebx, 1         ; B = B/2 don't need this because I'm doing it early
   loop WhileGreater
 
 Equal:                ; Exit if B == 0
